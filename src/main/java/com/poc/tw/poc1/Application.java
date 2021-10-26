500cd5e0a415c36b1213d16a4a925891fb27f9df
xupengma <xupeng.ma@thoughtworks.com>
2021-10-26 15:31:25 +0800

    modify contents

--- src/main/java/com/poc/tw/poc1/Application.java
+++ src/main/java/com/poc/tw/poc1/Application.java
@@ -2,12 +2,26 @@ package com.poc.tw.poc1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@RestController
+	class EchoController{
+
+		@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+		public String echo(@PathVariable String msg){
+			return "Hello Nacos Discovery " + msg;
+		}
+	}
 }
